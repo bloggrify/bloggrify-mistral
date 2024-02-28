975338e8bf87f806f75c1443a675924cd01c449f
hlassiege <hugo@malt.com>
2024-02-28 23:08:41 +0100

    change repository

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    extends: ["../bloggr"],
+    extends: ["github:hlassiege/bloggr"],
 
     runtimeConfig: {
         public: {

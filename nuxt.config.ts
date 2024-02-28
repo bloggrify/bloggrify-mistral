7411ef8c693ff2a3d25d92e1b5c1ccf19c4ff8a3
hlassiege <hugo@malt.com>
2024-02-28 23:07:28 +0100

    fix eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-    extends: ['../bloggr'],
+    extends: ["../bloggr"],
 
     runtimeConfig: {
         public: {

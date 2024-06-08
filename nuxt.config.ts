f3a474f74cc280b495991203f2122f110cd92b9a
hlassiege <hlassiege@gmail.com>
2024-06-08 17:03:45 +0200

    chore: remove configuration that is now useless

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            script: [
-                ...(process.env.PIRSCH_CODE
-                    ? [
-                        {
-                            src: 'https://api.pirsch.io/pirsch.js',
-                            id: 'pirschjs',
-                            defer: true,
-                            'data-code': process.env.PIRSCH_CODE,
-                            type: 'text/javascript',
-                        },
-                    ]
-                    : []),
-            ],
-        },
-    },
     extends: [
         '@bloggrify/core',
     ],

3c9395617c77dece7609c6a294ea8aba9942bb8e
Hugo Lassiège <hugo@malt.com>
2025-01-24 12:23:15 +0900

    chore: update dependency to bloggrify core 2.0.0

--- app.config.ts
+++ app.config.ts
@@ -1,13 +1,6 @@
 export default defineAppConfig({
     theme: 'mistral',
 
-    comments: {
-        enabled: true,
-        hyvor_talk: {
-            website_id: '10519', // Replace with your Hyvor Talk website ID (it won't work with this one)
-        },
-    },
-
     authors: [
         {
             name: 'John Doe',
